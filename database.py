adecf4d1562e99bc3e119c0aaf115e030c9a95f3
Eira Paakkunainen <126657543+eirapaakkunainen@users.noreply.github.com>
2026-02-04 13:00:59 +0200

    add database.py

---
+++ database.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Place(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    peopleCount = db.Column(db.Integer, nullable=False)
+
+    queue = db.Relationship("Queue", back_populates = "place")
+    queue = db.Relationship("User", back_populates = "place")
+
+
+class Queue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    queue_type = db.Column(db.String(20), nullable = True)
+    peopleCount = db.Column(db.Int, nullable=False)
+    place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
+
+    place = db.Relationship("Place", back_populates = "queue")
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(60), nullable = False)
+    place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
+
+    place = db.Relationship("Place", back_populates = "user")
