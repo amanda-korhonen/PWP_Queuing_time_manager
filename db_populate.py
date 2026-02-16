c3fcf0bd613806036579fe73b2f3abf536c0c8bd
alpikii <98106894+alpikii@users.noreply.github.com>
2026-02-16 08:48:40 +0000

    Add place type and location

--- db_populate.py
+++ db_populate.py
@@ -4,12 +4,12 @@ ctx = app.app_context()
 ctx.push()
 db.create_all()
 
-place1 = Place(name="Bar1", capacity=50, peopleCount=30)
-place2 = Place(name="Club1", capacity=200, peopleCount=120)
-place3 = Place(name="Bar2", capacity=100, peopleCount=100)
+place1 = Place(name="Bar1", capacity=50, peopleCount=30, place_type="Bar", location="City1")
+place2 = Place(name="Club1", capacity=200, peopleCount=120, place_type="Club", location="City1")
+place3 = Place(name="Bar2", capacity=100, peopleCount=100, place_type="Bar", location="City1")
 
-queue1 = Queue(peopleCount=0, place=place1)
-queue2 = Queue(peopleCount=100, place=place2)
+queue1 = Queue(queue_type="General", peopleCount=0, place=place1)
+queue2 = Queue(queue_type="VIP", peopleCount=100, place=place2)
 queue3 = Queue(peopleCount=50, place=place3)
 
 user1 = User(password="password1", place=place1)
