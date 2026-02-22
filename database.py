8c41d8fb8081dc3093484fca6f5a10f5bcaf6f9a
Eira Paakkunainen <eira.qiaoqiao@gmail.com>
2026-02-22 16:56:43 +0200

    updated class Queue(db.Model)
    added serialize, deserialize and json_schema for DL3

--- database.py
+++ database.py
@@ -26,6 +26,42 @@ class Queue(db.Model):
 
     place = db.relationship("Place", back_populates = "queue")
 
+    '''
+    The functions serialize, deserialize and json_schema were created based on the following references:
+    - Sensor class of the sensorhub example
+    https://github.com/UniOulu-Ubicomp-Programming-Courses/pwp-sensorhub-example/blob/ex2-05-validation/app.py 
+    - Chapters Serial Modeling, Embedded Serial Modeling, Deserial Modeling and Dynamic Schemas, Static Methods from course Lovelace 
+    https://lovelace.oulu.fi/ohjelmoitava-web/ohjelmoitava-web/implementing-rest-apis-with-flask/#embedded-serial-modeling
+
+    '''
+    def serialize(self):
+        return {
+            "queue_type": self.queue_type,
+            "people_count": self.people_count,
+            "place": self.place and self.place.name
+        }
+    
+    def deserialize(self, doc):
+        self.queue_type = doc["queue_type"]
+        self.people_count = doc["people_count"]
+
+    @staticmethod
+    def json_schema():
+        schema = {
+            "type": "object",
+            "required": ["queue_type", "people_count"]
+        }
+        props = schema["properties"] = {}
+        props["queue_type"] = {
+            "description": "The type of the queue, e.g. VIP",
+            "type": "string"
+        }
+        props["people"] = {
+            "description": "The muber of people in the queue",
+            "type": "integer"
+        }
+        return schema
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(60), nullable = False)
