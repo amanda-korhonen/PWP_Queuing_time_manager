ff979cd838270bf97b2b4cbad4a569e693b6e925
alpikii <98106894+alpikii@users.noreply.github.com>
2026-02-23 09:34:44 +0000

    correct comments

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
+"""Database, database models and their functions for the project."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
@@ -7,6 +9,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Place(db.Model):
+    '''A class to represent a place, e.g. restaurant, library, etc.'''
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
@@ -18,17 +22,19 @@ class Place(db.Model):
     user = db.relationship("User", cascade="all, delete-orphan", back_populates = "place")
 
     '''
-    The functions serialize, deserialize and json_schema were created based on the following references:
+    The functions serialize, deserialize and json_schema were created based on the
+    following references:
     - Sensor class of the sensorhub example
     https://github.com/UniOulu-Ubicomp-Programming-Courses/pwp-sensorhub-example/blob/ex2-05-validation/app.py 
-    - Chapters Serial Modeling, Embedded Serial Modeling, Deserial Modeling and Dynamic Schemas, Static Methods from course Lovelace 
+    - Chapters Serial Modeling, Embedded Serial Modeling, Deserial Modeling and Dynamic Schemas,
+    Static Methods from course Lovelace 
     https://lovelace.oulu.fi/ohjelmoitava-web/ohjelmoitava-web/implementing-rest-apis-with-flask/#embedded-serial-modeling
-    We changed the variable names in the fucntions and the json schema to match our project, but the structure of the functions is based on the references.
-
+    We changed the variable names in the fucntions to match our project, 
+    but the structure of the functions is based on the references.
     '''
 
-    #A method to change the variables to the correct format to send to the client
     def serialize(self):
+        '''This function is to change the variables to the correct format to send to the client.'''
         return {
             "name": self.name,
             "capacity": self.capacity,
@@ -36,18 +42,18 @@ class Place(db.Model):
             "place_type": self.place_type,
             "location": self.location
         }
-    
-    #A method to change the variables to the correct format to save to the database
+
     def deserialize(self, doc):
+        '''A method to change the variables to the correct format to save to the database.'''
         self.name = doc["name"]
         self.capacity = doc["capacity"]
         self.people_count = doc["people_count"]
         self.place_type = doc["place_type"]
         self.location = doc["location"]
-    
-    # the sructure of the json data that the server accepts
+
     @staticmethod
     def json_schema():
+        '''the sructure of the json data that the server accepts'''
         schema = {
             "type": "object",
             "required": ["name", "capacity", "people_count", "place_type", "location"]
@@ -77,6 +83,7 @@ class Place(db.Model):
 
 
 class Queue(db.Model):
+    '''A class to represent a queue, e.g. VIP queue, regular queue, etc.'''
     id = db.Column(db.Integer, primary_key=True)
     queue_type = db.Column(db.String(20), nullable = True)
     people_count = db.Column(db.Integer, nullable=False)
@@ -85,30 +92,33 @@ class Queue(db.Model):
     place = db.relationship("Place", back_populates = "queue")
 
     '''
-    The functions serialize, deserialize and json_schema were created based on the following references:
+    The functions serialize, deserialize and json_schema were created based on the
+    following references:
     - Sensor class of the sensorhub example
     https://github.com/UniOulu-Ubicomp-Programming-Courses/pwp-sensorhub-example/blob/ex2-05-validation/app.py 
-    - Chapters Serial Modeling, Embedded Serial Modeling, Deserial Modeling and Dynamic Schemas, Static Methods from course Lovelace 
+    - Chapters Serial Modeling, Embedded Serial Modeling, Deserial Modeling and Dynamic Schemas,
+    Static Methods from course Lovelace 
     https://lovelace.oulu.fi/ohjelmoitava-web/ohjelmoitava-web/implementing-rest-apis-with-flask/#embedded-serial-modeling
-    We changed the variable names in the fucntions and the json schema to match our project, but the structure of the functions is based on the references.
-
+    We changed the variable names in the fucntions to match our project, 
+    but the structure of the functions is based on the references.
     '''
-    #A method to change the variables to the correct format to send to the client
+
     def serialize(self):
+        '''A method to change the variables to the correct format to send to the client'''
         return {
             "queue_type": self.queue_type,
             "people_count": self.people_count,
             "place": self.place and self.place.name
         }
-    
-    #A method to change the variables to the correct format to save to the database
+
     def deserialize(self, doc):
+        '''A method to change the variables to the correct format to save to the database'''
         self.queue_type = doc["queue_type"]
         self.people_count = doc["people_count"]
 
-    # the sructure of the json data that the server accepts
     @staticmethod
     def json_schema():
+        '''the sructure of the json data that the server accepts'''
         schema = {
             "type": "object",
             "required": ["queue_type", "people_count"]
@@ -125,6 +135,7 @@ class Queue(db.Model):
         return schema
 
 class User(db.Model):
+    '''A class to represent a user, e.g. restaurant owner etc.'''
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(60), nullable = False)
     place_id = db.Column(db.Integer, db.ForeignKey("place.id", ondelete="CASCADE"), nullable=False)
