79070e3919a225538d1053928682e0adac5b5f6b
topakka <tpuro19@student.oulu.fi>
2026-02-23 11:40:27 +0200

    Modified Place.py

--- resources/Place.py
+++ resources/Place.py
@@ -1,3 +1,8 @@
+'''
+Place Collection and Item classes
+
+#TODO fullness apufunktio tänne tai jonnekki muualle
+'''
 from flask import request, Response, url_for
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
@@ -6,14 +11,10 @@ from jsonschema import ValidationError, validate
 
 from database import db, Place
 
-'''
-Place Collection and Item classes
-
-#TODO fullness apufunktio tänne tai jonnekki muualle
-'''
 class PlaceCollection(Resource):
     
     def get(self):
+        """Get method for PlaceCollection."""
         response = []
         places = Place.query.all()
         for place in places:
@@ -21,23 +22,20 @@ class PlaceCollection(Resource):
         return response, 200
     
     def post(self):
+        """Post method for PlaceCollection."""
         if not request.json:
             raise UnsupportedMediaType
 
-        # Placeholder for schema-validation
         try:
-            place = Place(
-                name=request.json["name"],
-                capacity=request.json["capacity"],
-                people_count=request.json["people_count"],
-                place_type=request.json["place_type"],
-                location=request.json["location"]
-            )
+            validate(request.json, Place.json_schema())
+        except ValidationError as e:
+            raise BadRequest(description = str(e))
 
+        place = Place()
+        place.deserialize(request.json)
+        try:
             db.session.add(place)
             db.session.commit()
-        except KeyError as e:
-            raise BadRequest(description = str(e))
         except IntegrityError:
             raise Conflict(
                 description="Place with name '{name}' already exists.".format(
@@ -47,10 +45,11 @@ class PlaceCollection(Resource):
         return Response(status=201, headers= {
             "Location": url_for("api.placeitem", place=place)
         })
-            
+
 class PlaceItem(Resource):
     
     def get(self, place):
+        """Get method for place."""
         return {
             "name": place.name,
             "capacity": place.capacity,
@@ -62,6 +61,7 @@ class PlaceItem(Resource):
     # NOTE: Mikäli aikaa implementoida admin oikeus
     # @require_adimn
     def put(self, place):
+        """Put method for place."""
         if not request.json:
             raise UnsupportedMediaType
         try:
@@ -85,7 +85,7 @@ class PlaceItem(Resource):
     # NOTE: Mikäli aikaa implementoida admin oikeus
     # @require_adimn
     def delete(self, place):
+        """Delete method for place."""
         db.session.delete(place)
         db.session.commit()
         return Response(status=204) # Deleted
-
